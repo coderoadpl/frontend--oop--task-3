3860cf90dcf3c77409756d5dcb66e1c3cbe6072c
Mateusz Choma <chomamateusz@pop-os.localdomain>
2020-06-29 14:22:13 +0200

    Automatic commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const inc = function () {
+    this.number = this.number + 1
+}
+
+const status = function () {
+    console.log('Current number is: ' + this.number)
+    return this.number
+}
+
+// constructor function
+const Counter = function (name) { 
+    // this points at newly created  empty object
+    // when is used with new keyword
+    this.name = name
+    this.number = 0 
+    this.inc = inc
+    this.status = status
+}
+
+const counter1 = new Counter('counter1')
+const counter2 = new Counter('counter2')
+const counter3 = new Counter('counter3')
+const counter4 = new Counter('counter4')
+
+counter1.inc()
+counter2.inc()
+counter2.inc()
+if (counter1.status() !== 1) throw new Error('Counter 1 do not work')
+if (counter2.status() !== 2) throw new Error('Counter 2 do not work')
